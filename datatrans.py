3732875db0fd59554660ca2ce4810c30097c7690
’Anastasiya <nastyanelchyk@ukr.net>
2013-05-15 23:20:28 +0300

    Defenition of myfunc

--- datatrans.py
+++ datatrans.py
@@ -6,3 +6,9 @@ try:
 except:
 	print ("Usage:", sys.argv[0], "infile outfile")
 	sys.exit(1)
+ofile = open(outfilename, 'w')
+def myfunc(y):
+    if y >= 0.0:
+        return y**5*math.exp(-y)
+    else:
+        return 0.0
